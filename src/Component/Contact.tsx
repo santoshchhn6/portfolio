c466d0cb1cdb452704329232f51083486937e505
Santosh Chauhan <santoshchhn6@gmail.com>
2023-03-05 14:36:46 +0530

    contact style completed

--- src/Component/Contact.tsx
+++ src/Component/Contact.tsx
@@ -3,6 +3,7 @@ import { style } from "../style";
 import { MdEmail } from "react-icons/md";
 import { AiFillPhone } from "react-icons/ai";
 import { FaMapMarkerAlt } from "react-icons/fa";
+import * as MaterialDesign from "react-icons/md";
 
 const Contact = () => {
   const handleSendMessage = (
@@ -14,11 +15,59 @@ const Contact = () => {
     console.log({ data });
   };
   return (
-    <div id="contact" className=" section">
-      <div className={`${style.heading}`}>Contacts</div>
-      <div className="flex max-sm:flex-col">
+    <div id="contact" className="border section">
+      <h2 className={`${style.heading} text-center`}>Contacts</h2>
+      <div className=" flex flex-wrap max-sm:flex-col max-sm:items-center">
         <UserInfo />
         <SendMessage handleSendMessage={handleSendMessage} />
+      </div>
+    </div>
+  );
+};
+
+const UserInfo = () => {
+  const info = [
+    {
+      title: "Email",
+      value: "santoshchhn6.com",
+      icon: "MdEmail",
+    },
+    {
+      title: "Phone",
+      value: "+91 8591267834",
+      icon: "MdPhone",
+    },
+    {
+      title: "Address",
+      value: "Mumbai, India",
+      icon: "MdLocationOn",
+    },
+  ];
+  return (
+    <div className="max-sm:w-[90%] flex-1 flex flex-col justify-center gap-5 my-5 ">
+      {info.map((e) => (
+        <CustomIcon title={e.title} icon={e.icon} value={e.value} />
+      ))}
+    </div>
+  );
+};
+
+type IconProps = {
+  title: string;
+  icon: string;
+  value: string;
+};
+const CustomIcon = ({ title, icon, value }: IconProps) => {
+  const MdIcon = MaterialDesign[icon];
+  return (
+    <div className="flex items-center gap-2">
+      <MdIcon
+        size={60}
+        className="text-blue-600 border-r-2 border-gray-500 pr-4 mr-3"
+      />
+      <div>
+        <p className="font-bold text-gray-900">{title}</p>
+        <p className="text-gray-800">{value}</p>
       </div>
     </div>
   );
@@ -39,7 +88,7 @@ const SendMessage = ({ handleSendMessage }: messageProps) => {
   };
   return (
     <form
-      className="flex flex-col items-center gap-4 "
+      className=" min-w-[300px] max-sm:w-[90%] flex-1 flex flex-col  gap-4 "
       onSubmit={(e) => handleSendMessage(e, inputs)}
     >
       <input
@@ -64,28 +113,11 @@ const SendMessage = ({ handleSendMessage }: messageProps) => {
         rows={5}
         onChange={(e) => handleInputs(e)}
       ></textarea>
-      <button type="submit" className={`${style.btn}`}>
+      <button type="submit" className={`${style.btn} m-0 w-fi`}>
         Send Message
       </button>
     </form>
   );
 };
-
-const UserInfo = () => (
-  <div className="flex flex-col gap-5 font-bold">
-    <div className="flex items-center gap-2">
-      <MdEmail size={40} />
-      <span>santoshchh6@gmail.com</span>
-    </div>
-    <div className="flex items-center gap-2">
-      <AiFillPhone size={40} />
-      <span>+91 8591267834</span>
-    </div>
-    <div className="flex items-center gap-2">
-      <FaMapMarkerAlt size={40} />
-      <span>Mumbai, India</span>
-    </div>
-  </div>
-);
 
 export default Contact;
--- src/style.tsx
+++ src/style.tsx
@@ -3,5 +3,6 @@ export const style = {
   btn2: "m-1 p-3 rounded-md font-bold ease-in  duration-300 hover:scale-105 border border-blue-600 text-blue-600  bg-white active:scale-95",
   btn3: "border bg-blue-600 text-white font-bold p-3 rounded-md hover:bg-blue-500 active:scale-95 ease-in-out duration-300",
   heading: "text-blue-600 p-2 font-bold text-2xl  my-5",
-  inp: " bg-gray-200 outline-none w-[90%] p-3 focus:border-b-4 focus:border-blue-500",
+  inp: " bg-gray-200 outline-none w-[100%] p-3 focus:border-b-4 focus:border-blue-500",
+  icon: "border-r-2 border-gray-500 px-4 mr-3",
 };
